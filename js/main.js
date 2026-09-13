245846910ba86dcc84f2f1e82395ba7c08c6cb86
yj-min-i <bellayjm06@naver.com>
2026-09-13 18:32:09 +0900

    모바일 메뉴 링크 클릭시 자동 닫힘 추가

--- js/main.js
+++ js/main.js
@@ -4,3 +4,9 @@ const navMenu = document.querySelector('.nav-menu');
 hamburger.addEventListener('click', () => {
   navMenu.classList.toggle('active');
 });
+
+document.querySelectorAll('.nav-menu a').forEach((link) => {
+  link.addEventListener('click', () => {
+    navMenu.classList.remove('active');
+  });
+});
